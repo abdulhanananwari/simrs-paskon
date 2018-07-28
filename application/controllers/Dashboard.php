7f782d948e6194601677c49bc68ea9e6be847415
abdulhanan <abdulhananx42@gmail.com>
2018-07-28 12:12:12 +0700

    fixed dashboard view

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -2,7 +2,15 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Dashboard extends CI_Controller {
-
+	function __construct()
+    {
+        parent::__construct();
+		$this->load->model('Pasien_model');
+		$this->load->model('Dokter_model');
+		$this->load->model('Ruangan_model');
+		$this->load->model('Poliklinik_model');
+        $this->load->library('form_validation');
+    }
 	/**
 	 * Index Page for this controller.
 	 *
@@ -24,7 +32,13 @@ class Dashboard extends CI_Controller {
 	}
 	public function isLogin(){
 		if($this->session->userdata('login') == TRUE){
-			$this->template->load('template', 'dashboard');
+			
+			$data['pasien'] = $this->Pasien_model->total_rows();
+			$data['dokter'] = $this->Dokter_model->total_rows();
+			$data['ruangan'] = $this->Ruangan_model->total_rows();
+			$data['poliklinik'] = $this->Ruangan_model->total_rows();
+
+			$this->template->load('template', 'dashboard',$data);
 		}else {
 			redirect('login');
 		}
--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -10,44 +10,44 @@
                 <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
                     <div class="info-box bg-pink hover-expand-effect">
                         <div class="icon">
-                            <i class="material-icons">playlist_add_check</i>
+                            <i class="material-icons">person</i>
                         </div>
                         <div class="content">
-                            <div class="text">NEW TASKS</div>
-                            <div class="number count-to" data-from="0" data-to="125" data-speed="15" data-fresh-interval="20"></div>
+                            <div class="text">Total Pasien</div>
+                            <div class="number count-to" data-from="0" data-to="<?php echo $pasien;?>" data-speed="15" data-fresh-interval="20"></div>
                         </div>
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
                     <div class="info-box bg-cyan hover-expand-effect">
                         <div class="icon">
-                            <i class="material-icons">help</i>
+                            <i class="material-icons">supervised_user_circle</i>
                         </div>
                         <div class="content">
-                            <div class="text">NEW TICKETS</div>
-                            <div class="number count-to" data-from="0" data-to="257" data-speed="1000" data-fresh-interval="20"></div>
+                            <div class="text">Dokter</div>
+                            <div class="number count-to" data-from="0" data-to="<?php echo $dokter;?>" data-speed="1000" data-fresh-interval="20"></div>
                         </div>
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
                     <div class="info-box bg-light-green hover-expand-effect">
                         <div class="icon">
-                            <i class="material-icons">forum</i>
+                            <i class="material-icons">hotel</i>
                         </div>
                         <div class="content">
-                            <div class="text">NEW COMMENTS</div>
-                            <div class="number count-to" data-from="0" data-to="243" data-speed="1000" data-fresh-interval="20"></div>
+                            <div class="text">Total Ruangan</div>
+                            <div class="number count-to" data-from="0" data-to="<?php echo $ruangan;?>" data-speed="1000" data-fresh-interval="20"></div>
                         </div>
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
                     <div class="info-box bg-orange hover-expand-effect">
                         <div class="icon">
-                            <i class="material-icons">person_add</i>
+                            <i class="material-icons">local_hospital</i>
                         </div>
                         <div class="content">
-                            <div class="text">NEW VISITORS</div>
-                            <div class="number count-to" data-from="0" data-to="1225" data-speed="1000" data-fresh-interval="20"></div>
+                            <div class="text">Poliklinik</div>
+                            <div class="number count-to" data-from="0" data-to="<?php echo $poliklinik;?>" data-speed="1000" data-fresh-interval="20"></div>
                         </div>
                     </div>
                 </div>
